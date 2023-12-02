7b4c801e707fc71e9f8d3bfefea3fdc7d6c6070b
dangnhatminh <dangnhatminh09032002@gmail.com>
2023-12-02 07:32:03 +0700

    chore(other): :shrug: update

--- env/env.api.mjs
+++ env/env.api.mjs
@@ -5,9 +5,9 @@ export default {
 		key_store_access_token: 'access_token',
 		meeting_token_header: 'meeting-api-token',
 
-		base_url: 'https://www.gdscmeet.live/api',
-		login_google_url: 'https://www.gdscmeet.live/api/auth/google/login',
-		webrtc_socket_url: 'https://www.gdscmeet.live/rtc-api',
+		base_url: 'https://www.gdscmeet.live:5000',
+		login_google_url: 'https://www.gdscmeet.live:5000/auth/google/login',
+		webrtc_socket_url: 'https://www.gdscmeet.live:7880',
 		// base_url: 'http://localhost:5000',
 		// login_google_url: 'http://localhost:5000/auth/google/login',
 		// webrtc_socket_url: 'http://localhost:7880',
